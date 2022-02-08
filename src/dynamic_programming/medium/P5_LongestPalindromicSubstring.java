ae46b4bdadb03310dfbcda0ddf15dc4747552e9f
chengzhy <951241690@qq.com>
2022-02-08 11:00:27 +0800

    题5-最长回文子串(更新题解，参考官方解答)

--- src/dynamic_programming/medium/P5_LongestPalindromicSubstring.java
+++ src/dynamic_programming/medium/P5_LongestPalindromicSubstring.java
@@ -22,7 +22,7 @@ package dynamic_programming.medium;
  */
 public class P5_LongestPalindromicSubstring {
     public String longestPalindrome(String s) {
-        if (s.length() == 0) {
+        /*if (s.length() == 0) {
             return s;
         }
         char[] temp = new char[2*s.length()+4];
@@ -65,7 +65,37 @@ public class P5_LongestPalindromicSubstring {
                 break;
             }
         }
-        return sb.toString();
+        return sb.toString();*/
+        /**
+         * 边界情况即为子串长度为1或2的情况。我们枚举每一种边界情况，并从对应的子串开始不断地向两边扩展。
+         * 如果两边的字母相同，我们就可以继续扩展，例如从P(i+1,j-1)扩展到 P(i,j)；
+         * 如果两边的字母不同，我们就可以停止扩展，因为在这之后的子串都不能是回文串了。
+         * 聪明的读者此时应该可以发现，「边界情况」对应的子串实际上就是我们「扩展」出的回文串的「回文中心」。
+         * 方法二的本质即为：我们枚举所有的「回文中心」并尝试「扩展」，直到无法扩展为止，此时的回文串长度即为此「回文中心」下的最长回文串长度。
+         * 我们对所有的长度求出最大值，即可得到最终的答案。
+         */
+        if (s.length() < 1) {
+            return s;
+        }
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    public int expandAroundCenter(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            --left;
+            ++right;
+        }
+        return right - left - 1;
     }
 
 }
