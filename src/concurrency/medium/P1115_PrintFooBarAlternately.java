f7ea57670e1c32fee5b6c75a10cc35422558582a
chengzhy <951241690@qq.com>
2022-02-09 09:56:38 +0800

    题1115-交替打印 FooBar(多线程，使用Semaphore控制)

---
+++ src/concurrency/medium/P1115_PrintFooBarAlternately.java
@@ -0,0 +1,80 @@
+package concurrency.medium;
+
+import java.util.concurrent.Semaphore;
+
+/**
+ * 交替打印 FooBar
+ *
+ * 给你一个类：
+ *
+ * class FooBar {
+ *   public void foo() {
+ *     for (int i = 0; i < n; i++) {
+ *       print("foo");
+ *     }
+ *   }
+ *
+ *   public void bar() {
+ *     for (int i = 0; i < n; i++) {
+ *       print("bar");
+ *     }
+ *   }
+ * }
+ * 两个不同的线程将会共用一个 FooBar 实例：
+ *
+ * 线程 A 将会调用 foo() 方法，而
+ * 线程 B 将会调用 bar() 方法
+ * 请设计修改程序，以确保 "foobar" 被输出 n 次。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：n = 1
+ * 输出："foobar"
+ * 解释：这里有两个线程被异步启动。其中一个调用 foo() 方法, 另一个调用 bar() 方法，"foobar" 将被输出一次。
+ * 示例 2：
+ *
+ * 输入：n = 2
+ * 输出："foobarfoobar"
+ * 解释："foobar" 将被输出两次。
+ *  
+ *
+ * 提示：
+ *
+ * 1 <= n <= 1000
+ *
+ * @author chengzhy
+ * @date 2022/2/9 9:36
+ */
+public class P1115_PrintFooBarAlternately {
+
+    public static class FooBar {
+        private int n;
+        private Semaphore foo = new Semaphore(1);
+        private Semaphore bar = new Semaphore(0);
+
+        public FooBar(int n) {
+            this.n = n;
+        }
+
+        public void foo(Runnable printFoo) throws InterruptedException {
+            for (int i = 0; i < n; i++) {
+                foo.acquire();
+                // printFoo.run() outputs "foo". Do not change or remove this line.
+                printFoo.run();
+                bar.release();
+            }
+        }
+
+        public void bar(Runnable printBar) throws InterruptedException {
+            for (int i = 0; i < n; i++) {
+                bar.acquire();
+                // printBar.run() outputs "bar". Do not change or remove this line.
+                printBar.run();
+                foo.release();
+            }
+        }
+    }
+
+}
