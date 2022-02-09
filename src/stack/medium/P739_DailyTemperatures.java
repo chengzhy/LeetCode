1fba51dedce5021f9e61ff7b299d25d5a20efa78
chengzhy <951241690@qq.com>
2022-02-09 13:42:48 +0800

    题739-每日温度(单调栈，暴力双循环太慢，单调栈参考题解)

---
+++ src/stack/medium/P739_DailyTemperatures.java
@@ -0,0 +1,49 @@
+package stack.medium;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * 每日温度
+ *
+ * 请根据每日 气温 列表 temperatures ，请计算在每一天需要等几天才会有更高的温度。如果气温在这之后都不会升高，请在该位置用 0 来代替。
+ *
+ * 示例 1:
+ *
+ * 输入: temperatures = [73,74,75,71,69,72,76,73]
+ * 输出: [1,1,4,2,1,1,0,0]
+ * 示例 2:
+ *
+ * 输入: temperatures = [30,40,50,60]
+ * 输出: [1,1,1,0]
+ * 示例 3:
+ *
+ * 输入: temperatures = [30,60,90]
+ * 输出: [1,1,0]
+ *  
+ *
+ * 提示：
+ *
+ * 1 <= temperatures.length <= 105
+ * 30 <= temperatures[i] <= 100
+ *
+ * @author chengzhy
+ * @date 2022/2/9 12:47
+ */
+public class P739_DailyTemperatures {
+
+    public int[] dailyTemperatures(int[] temperatures) {
+        int[] result = new int[temperatures.length];
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (int i = 0; i < temperatures.length; i++) {
+            // 单调栈，不用暴力，找到比栈中大的就把栈中的弹出去
+            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                int index = stack.pop();
+                result[index] = i - index;
+            }
+            stack.push(i);
+        }
+        return result;
+    }
+
+}
