82e92d24c9d1090a34d46178aa9c02adb993894e
chengzhy <951241690@qq.com>
2022-02-09 09:24:29 +0800

    题2006-差的绝对值为 K 的数对数目(用哈希表优化暴力解法)

---
+++ src/hashtable/easy/P2006_CountNumberOfPairsWithAbsoluteDifferenceK.java
@@ -0,0 +1,69 @@
+package hashtable.easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 差的绝对值为 K 的数对数目
+ *
+ * 给你一个整数数组 nums 和一个整数 k ，请你返回数对 (i, j) 的数目，满足 i < j 且 |nums[i] - nums[j]| == k 。
+ *
+ * |x| 的值定义为：
+ *
+ * 如果 x >= 0 ，那么值为 x 。
+ * 如果 x < 0 ，那么值为 -x 。
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [1,2,2,1], k = 1
+ * 输出：4
+ * 解释：差的绝对值为 1 的数对为：
+ * - [1,2,2,1]
+ * - [1,2,2,1]
+ * - [1,2,2,1]
+ * - [1,2,2,1]
+ * 示例 2：
+ *
+ * 输入：nums = [1,3], k = 3
+ * 输出：0
+ * 解释：没有任何数对差的绝对值为 3 。
+ * 示例 3：
+ *
+ * 输入：nums = [3,2,1,5,4], k = 2
+ * 输出：3
+ * 解释：差的绝对值为 2 的数对为：
+ * - [3,2,1,5,4]
+ * - [3,2,1,5,4]
+ * - [3,2,1,5,4]
+ *  
+ *
+ * 提示：
+ *
+ * 1 <= nums.length <= 200
+ * 1 <= nums[i] <= 100
+ * 1 <= k <= 99
+ *
+ * @author chengzhy
+ * @date 2022/2/9 9:15
+ */
+public class P2006_CountNumberOfPairsWithAbsoluteDifferenceK {
+
+    public int countKDifference(int[] nums, int k) {
+        int count = 0;
+        /*for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (Math.abs(nums[i] - nums[j]) == k) {
+                    count++;
+                }
+            }
+        }*/
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            count += map.getOrDefault(nums[i] - k, 0) + map.getOrDefault(nums[i] + k, 0);
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        }
+        return count;
+    }
+
+}
