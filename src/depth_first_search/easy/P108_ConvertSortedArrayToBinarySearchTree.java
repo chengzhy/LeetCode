fc2d5302d3ead9bb1036996032614a119b7a3319
chengzhy <951241690@qq.com>
2022-03-15 14:48:45 +0800

    题108-将有序数组转换为二叉搜索树(dfs，区间递归，选左半区间作为root的左子树，选右半区间作为root的右子树)

---
+++ src/depth_first_search/easy/P108_ConvertSortedArrayToBinarySearchTree.java
@@ -0,0 +1,71 @@
+package depth_first_search.easy;
+
+/**
+ * 将有序数组转换为二叉搜索树
+ *
+ * 给你一个整数数组 nums ，其中元素已经按 升序 排列，请你将其转换为一棵 高度平衡 二叉搜索树。
+ *
+ * 高度平衡 二叉树是一棵满足「每个节点的左右两个子树的高度差的绝对值不超过 1 」的二叉树。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：nums = [-10,-3,0,5,9]
+ * 输出：[0,-3,9,-10,null,5]
+ * 解释：[0,-10,5,null,-3,null,9] 也将被视为正确答案：
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：nums = [1,3]
+ * 输出：[3,1]
+ * 解释：[1,null,3] 和 [3,1] 都是高度平衡二叉搜索树。
+ *  
+ *
+ * 提示：
+ *
+ * 1 <= nums.length <= 104
+ * -104 <= nums[i] <= 104
+ * nums 按 严格递增 顺序排列
+ *
+ * @author chengzhy
+ * @date 2022/3/15 14:30
+ */
+public class P108_ConvertSortedArrayToBinarySearchTree {
+
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return dfs(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode dfs(int[] nums, int left, int right) {
+        if (left > right) {
+            return null;
+        }
+        int mid = (left + right) >> 1;
+        // 取中点位置作为root
+        TreeNode root = new TreeNode(nums[mid]);
+        // 因为数组为排序后的，所以选左半区间作为root的左子树
+        root.left = dfs(nums, left, mid - 1);
+        // 选右半区间作为root的右子树
+        root.right = dfs(nums, mid + 1, right);
+        return root;
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) {
+            this.val = val;
+        }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+}
