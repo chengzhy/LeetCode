16b0e53e1758dce9e94d5c7fc2ce48dd1f3c9db2
chengzhy <951241690@qq.com>
2022-04-07 11:45:00 +0800

    题416-分割等和子集(动态规划，01背包问题，难点在于怎么把问题转换成01背包解决，一开始还是看代码随想录的题解才能转换问题)

---
+++ src/dynamic_programming/medium/P416_PartitionEqualSubsetSum.java
@@ -0,0 +1,59 @@
+package dynamic_programming.medium;
+
+/**
+ * 分割等和子集
+ *
+ * 给你一个 只包含正整数 的 非空 数组 nums 。请你判断是否可以将这个数组分割成两个子集，使得两个子集的元素和相等。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [1,5,11,5]
+ * 输出：true
+ * 解释：数组可以分割成 [1, 5, 5] 和 [11] 。
+ * 示例 2：
+ *
+ * 输入：nums = [1,2,3,5]
+ * 输出：false
+ * 解释：数组不能分割成两个元素和相等的子集。
+ *  
+ *
+ * 提示：
+ *
+ * 1 <= nums.length <= 200
+ * 1 <= nums[i] <= 100
+ *
+ * @author chengzhy
+ * @date 2022/4/7 11:08
+ */
+public class P416_PartitionEqualSubsetSum {
+
+    public boolean canPartition(int[] nums) {
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        if ((sum & 1) == 0) {
+            sum >>= 1;
+            /**
+             * dp[j]数组表示容量为j的背包，所能放物品的最大价值是dp[j]
+             * 根据题意可以将其转换为01背包问题，从nums中(物品)找到能达到容量为sum / 2的子集即可
+             * 物品的重量和价值都等于nums[i]
+             */
+            int[] dp = new int[sum + 1];
+            // 遍历物品
+            for (int i = 0; i < nums.length; i++) {
+                // 遍历背包
+                for (int j = sum; j >= nums[i]; j--) {
+                    dp[j] = Math.max(dp[j], dp[j - nums[i]] + nums[i]);
+                    if (dp[j] == sum) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+}
